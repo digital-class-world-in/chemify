34c061576fd3ff0fdd8ccd6c8a90e2e7fa8e0aec
digital-class-world-in <digitalclassworldin@gmail.com>
2026-03-17 16:40:46 +0530

    v3

--- src/app/branch/staff/page.tsx
+++
@@ -1,8 +0,0 @@
-
-"use client"
-
-import EmployeeDirectoryPage from "@/app/hr/directory/page"
-
-export default function BranchStaffPage() {
-  return <EmployeeDirectoryPage />
-}
--- src/app/staff/staff-directory/page.tsx
+++
@@ -1,6 +0,0 @@
-
-"use client"
-import EmployeeDirectoryPage from "@/app/hr/directory/page"
-export default function StaffDirectoryPage() {
-  return <EmployeeDirectoryPage />
-}
